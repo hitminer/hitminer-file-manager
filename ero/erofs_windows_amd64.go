8261a2896e584da5adef68cb663ee26479b9ddb7
youngk1019 <1186354278@qq.com>
2023-05-29 23:18:43 +0800

    version 1.4.5: 修复windows下ero打包存在的bug

--- ero/erofs_windows_amd64.go
+++ ero/erofs_windows_amd64.go
@@ -12,6 +12,12 @@ import (
 //go:embed static/windows_amd64/mkfs.erofs.exe
 var erofs []byte
 
+//go:embed static/windows_amd64/cygwin1.dll
+var cygwin1 []byte
+
 func WriteErofs(path string) error {
-	return os.WriteFile(filepath.Join(path, "mkfs.erofs.exe"), erofs, 0755)
+	if err := os.WriteFile(filepath.Join(path, "mkfs.erofs.exe"), erofs, 0755); err != nil {
+		return err
+	}
+	return os.WriteFile(filepath.Join(path, "cygwin1.dll"), cygwin1, 0755)
 }
